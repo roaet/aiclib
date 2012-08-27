7a1919adc1d502583a7d7587c6241b233ff3ac86
Justin Hammond <justin.hammond@rackspace.com>
2012-08-27 15:45:48 -0500

    Fixed logging to not spam during unit testing

--- aic/log.py
+++ aic/log.py
@@ -5,11 +5,11 @@ def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-            '%(asctime)s - %(levelname)s - %(name)s - %(message)s')
-
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    #ch = logging.StreamHandler()
+    #ch.setLevel(logging.DEBUG)
+    #formatter = logging.Formatter(
+    #        '%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+    #
+    #ch.setFormatter(formatter)
+    #logger.addHandler(ch)
     return logger
