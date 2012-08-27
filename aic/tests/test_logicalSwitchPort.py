fc59fe3ecc3390c904aee17f6ea42a90faac48b4
Justin Hammond <justin.hammond@rackspace.com>
2012-08-27 15:34:16 -0500

    Added more tests for logical switch ports

--- aic/tests/test_logicalSwitchPort.py
+++ aic/tests/test_logicalSwitchPort.py
@@ -14,10 +14,13 @@ class TestLogicalSwitch(test.TestCase):
     def setUp(self):
         self.nvp = aic.nvp.Connection("https://nvp")
         self.switch = self.nvp.lswitch().create()
+        self.switchport = self.nvp.lswitch_port(self.switch).create()
 
     def tearDown(self):
+        if self.switchport:
+            self.nvp.lswitch_port(self.switchport).delete()
         if self.switch:
             self.nvp.lswitch(self.switch).delete()
 
     def test_switchport_create(self):
-        pass
+        self.assertTrue(True)
