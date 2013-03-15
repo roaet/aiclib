659bb52a587d8a76b8146f2a68c2ca12054e1b15
Jason Kölker <jason@koelker.net>
2013-03-15 00:13:36 -0500

    Add Apache License header and make standalone tests

--- aiclib/tests/__init__.py
+++ aiclib/tests/__init__.py
@@ -13,3 +13,19 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+"""
+Created: August 27, 2012
+
+@author: Justin Hammond, Rackspace Hosting
+"""
+import sys
+
+if sys.version_info >= (2, 7):
+    import unittest
+else:
+    import unittest2 as unittest
+
+
+class TestCase(unittest.TestCase):
+    pass
