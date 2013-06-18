d43302d48fc7ef580587bc5f95847a5bea8cc000
Jason Kölker <jason@koelker.net>
2013-06-18 11:34:14 -0500

    Version Bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import setuptools
 
 setuptools.setup(
     name='aiclib',
-    version="0.7",
+    version="0.8",
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
